de40bd25e963f1e75ecf665c0010a7db2baa7523
ProSS <ProSS96@163.com>
2017-10-16 22:44:55 +0800

    Site updated: 2017-10-16 22:44:52

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 05 2017 16:21:35 GMT+0800 (中国标准时间)
+// build time:Mon Oct 16 2017 22:44:45 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
