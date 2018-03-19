99a42c0eccf3917149f67fdc7d3984c05447c556
prosscode <pross96@163.com>
2018-03-19 22:59:45 +0800

    Site updated: 2018-03-19 22:59:43

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 19 2018 22:32:44 GMT+0800 (中国标准时间)
+// build time:Mon Mar 19 2018 22:59:37 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
