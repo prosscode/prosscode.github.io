ec98dc70bc08bdd7c94a27e427c13d841bc0291e
shuang.peng <rukiapross@foxmail.com>
2021-01-04 20:29:14 +0800

    Site updated: 2021-01-04 20:29:14

--- js/totop.js
+++ js/totop.js
@@ -1,12 +1,3 @@
-$(window).scroll(function() {
-    $(window).scrollTop() > 500 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
-});
-$("#rocket").click(function() {
-    $("#rocket").addClass("launch");
-    $("html, body").animate({
-        scrollTop: 0
-    }, 500, function() {
-        $("#rocket").removeClass("show launch");
-    });
-    return false;
-});
+// build time:Mon Aug 19 2019 23:01:12 GMT+0800 (中国标准时间)
+$(window).scroll(function(){$(window).scrollTop()>500?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},500,function(){$("#rocket").removeClass("show launch")});return false});
+//rebuild by neat 
